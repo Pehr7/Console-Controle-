9a82a5cc27d49bf1868582ba59648601120cfb63
Pehr7 <131710479+Pehr7@users.noreply.github.com>
2024-03-26 15:12:02 -0300

    Update Disable.php

--- Console/Command/Disable.php
+++ Console/Command/Disable.php
@@ -1,4 +1,4 @@
-<?php
+<?php   
 /**
  * Copyright © Magefan (support@magefan.com). All rights reserved.
  * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
